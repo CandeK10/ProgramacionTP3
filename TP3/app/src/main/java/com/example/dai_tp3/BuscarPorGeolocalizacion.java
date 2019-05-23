c0bac269bd45c61441daa9456f465df898682ac4
Android Studio <devandroid@ort>
2019-05-23 12:09:44 -0300

    ultimo coso

--- TP3/app/src/main/java/com/example/dai_tp3/BuscarPorGeolocalizacion.java
+++ TP3/app/src/main/java/com/example/dai_tp3/BuscarPorGeolocalizacion.java
@@ -1,7 +1,11 @@
 package com.example.dai_tp3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BuscarPorGeolocalizacion extends AppCompatActivity {
 
@@ -9,5 +13,41 @@ public class BuscarPorGeolocalizacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_por_geolocalizacion);
+    }
+
+    public void BuscarPorGeolocalizacion(View vistaRecibida){
+        EditText CoordenadaX, CoordenadaY, Radio;
+        CoordenadaX=findViewById(R.id.CoordenadaX);
+        CoordenadaY=findViewById(R.id.CoordenadaY);
+        Radio=findViewById(R.id.Radio);
+
+        String sCoordenadaX, sCoordenadaY, sRadio;
+        sCoordenadaX=CoordenadaX.getText().toString();
+        sCoordenadaY=CoordenadaY.getText().toString();
+        sRadio=Radio.getText().toString();
+
+        if(sCoordenadaX.equals("") || sCoordenadaY.equals("") || sRadio.equals("")){
+            TextView MensajeError;
+            MensajeError=findViewById(R.id.MensajeErrorPunto3);
+            MensajeError.setText("Ingrese todos los datos");
+        }else{
+            Float fCoordenadaX, fCoordenadaY;
+            Integer iRadio;
+            fCoordenadaX=Float.parseFloat(sCoordenadaX);
+            fCoordenadaY=Float.parseFloat(sCoordenadaY);
+            iRadio= Integer.parseInt(sRadio);
+
+            Bundle PaqueteDeDatos;
+            PaqueteDeDatos = new Bundle();
+            PaqueteDeDatos.putFloat("CoordenadaX", fCoordenadaX);
+            PaqueteDeDatos.putFloat("CoordenadaY", fCoordenadaY);
+            PaqueteDeDatos.putInt("Radio", iRadio);
+
+            Intent ActividadDestino;
+            ActividadDestino = new Intent(BuscarPorGeolocalizacion.this, MostrarObjetosPorGeolocalizacion.class);
+            ActividadDestino.putExtras(PaqueteDeDatos);
+
+            startActivity(ActividadDestino);
+        }
     }
 }
--- TP3/app/src/main/java/com/example/dai_tp3/MostrarObjetosPorNombre.java
+++ TP3/app/src/main/java/com/example/dai_tp3/MostrarObjetosPorNombre.java
@@ -3,10 +3,16 @@ package com.example.dai_tp3;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MostrarObjetosPorNombre extends AppCompatActivity {
@@ -41,11 +47,76 @@ public class MostrarObjetosPorNombre extends AppCompatActivity {
                 String miURL = "http://epok.buenosaires.gob.ar/buscar/?texto=" + Texto;
                 URL miRuta = new URL(miURL);
                 HttpURLConnection miConexion = (HttpURLConnection) miRuta.openConnection();
+                Log.d("PorNombre", "Me conecto");
+                if(miConexion.getResponseCode() == 200){
+                    Log.d("PorNombre", "Conexión OK");
+                    InputStream textoRespuesta = miConexion.getInputStream();
+                    InputStreamReader LectorRespuesta = new InputStreamReader(textoRespuesta, "UTF-8");
 
-
-            }catch{
-
+                    ObtenerObjetos(LectorRespuesta);
+                }
+                else{
+                    Log.d("PorNombre", "Error al conectar");
+                }
+                miConexion.disconnect();
+            }catch(Exception error){
+                Log.d("PorNombre", "El error es: "+error.getMessage());
             }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void aVoid){
+            super.onPostExecute(aVoid);
+            TextView Error;
+            Error = findViewById(R.id.MensajeError);
+            if(Objetos.isEmpty()){
+                Error.setText("No se han encontrado objetos");
+            }
+            else{
+                Error.setText("Los objetos encontrados son:");
+                ListaObjetos.setAdapter(Adaptador);
+            }
+        }
+    }
+
+    public void ObtenerObjetos(InputStreamReader LosDatos){
+        JsonReader DatosLeidos = new JsonReader(LosDatos);
+        try{
+            DatosLeidos.beginObject();
+            while(DatosLeidos.hasNext()){
+                String nombreElementoActual = DatosLeidos.nextName();
+                Log.d("NombresLectura", "El elemento actual es: "+nombreElementoActual);
+
+                if(nombreElementoActual.equals("totalFull")){
+                    DatosLeidos.skipValue();
+                }
+                if(nombreElementoActual.equals("clasesEncontradas")){
+                    DatosLeidos.skipValue();
+                }
+                if(nombreElementoActual.equals("instancias")){
+                    DatosLeidos.beginArray();
+                    while(DatosLeidos.hasNext()){
+                        DatosLeidos.beginObject();
+                        while (DatosLeidos.hasNext()){
+                            nombreElementoActual=DatosLeidos.nextName();
+                            if(nombreElementoActual.equals("nombre")){
+                                String NombreObjeto = DatosLeidos.nextString();
+                                Log.d("NombreSLectura", "Valor leido: "+NombreObjeto);
+                                Objetos.add(NombreObjeto);
+                            }
+                            else{
+                                DatosLeidos.skipValue();
+                            }
+                        }
+                        DatosLeidos.endObject();
+                    }
+                    DatosLeidos.endArray();
+                }
+            }
+            DatosLeidos.endObject();
+        }catch (Exception error){
+            Log.d("NombresLectura", "El error es: "+error.getMessage());
         }
     }
 }
